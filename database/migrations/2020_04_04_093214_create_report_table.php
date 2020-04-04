d5aa1367d955d6599bdf6cfcdfcb381bad650522
Nicolas Costa <nicolas.c.s.s@gmail.com>
2020-04-04 06:35:58 -0300

    Added:     * Database migrations.

---
+++ database/migrations/2020_04_04_093214_create_report_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReportTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('report', function (Blueprint $table) {
+            $table->id();
+            $table->integer('notified')->default(0);
+            $table->integer('suspicious')->default(0);
+            $table->integer('confirmed')->default(0);
+            $table->integer('discarded')->default(0);
+            $table->integer('deaths')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('report');
+    }
+}
